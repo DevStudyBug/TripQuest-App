4f7017aaa68031d4361bc45893f717ef2e526dbc
DevStudyBug <anshikadubey8591@gmail.com>
2025-11-10 14:17:00 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: '05-Trip-Quest-App',
+  webDir: 'www'
+};
+
+export default config;
